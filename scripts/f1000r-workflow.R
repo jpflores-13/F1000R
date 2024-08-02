62106ea0f9d0e8919f00ad2b80f3eb24bec2e465
Jp Flores <jpflores@c0300ood19.ll.unc.edu>
2024-08-02 16:01:05 -0400

    update workflow

--- scripts/f1000r-workflow.R
+++ scripts/f1000r-workflow.R
@@ -1,14 +1,3 @@
-## Install Packages
-BiocManager::install(c("mariner",
-                       "marinerData",
-                       "InteractionSet",
-                       "data.table",
-                       "plyranges",
-                       "apeglm",
-                       "DESeq2",
-                       "plotgardener",
-                       "RColorBrewer"))
-
 ## Load packages
 library(mariner)
 library(marinerData)
@@ -91,6 +80,8 @@ seqlevelsStyle(fsLoopsGI) <- "ENSEMBL"
 
 ## Create list of loops
 loopList <- list("WT" = wtLoopsGI, "FS" = fsLoopsGI)
+
+## View loops
 loopList
 
 ## Remove redundant loops (i.e. loops that are present in both WT & FS)
@@ -111,14 +102,13 @@ pixels <- pullHicPixels(
   x = mergedLoops,
   files = hicFiles,
   binSize = 10e3)
-
-## Show attributes of `pixels`
 pixels
 
 ## Show count matrix housed within `pixels`
 counts(pixels)
 
 ## Filter out loops with low counts (at least 10 counts in at least 4 samples)
+
 keep <- rowSums(counts(pixels) >= 10) >= 4
 pixels <- pixels[keep,]
 
@@ -132,6 +122,7 @@ rownames(colData) <- colnames(counts(pixels))
 ## Ensure the colnames of the count matrix is equal to the rownames of the colData
 all(colnames(counts(pixels)) == rownames(colData))
 
+
 ## Build a DESeq Dataset
 dds <- DESeqDataSetFromMatrix(
   countData = counts(pixels),
@@ -141,6 +132,7 @@ dds <- DESeqDataSetFromMatrix(
 ## Perform differential expression analysis based on the Negative Binomial (a.k.a. Gamma-Poisson) distribution
 dds <- DESeq(dds)
 dds
+
 
 ## Get shrunken results
 res <- lfcShrink(dds,
@@ -163,40 +155,36 @@ plotMA(res,
 rowData(pixels) <- res
 
 ## Separate WT/FS-specific loops 
-fsLoops <- pixels[rowData(pixels)$padj <= 0.05 & rowData(pixels)$log2FoldChange > 0]
+diff_fsLoops <- pixels[rowData(pixels)$padj <= 0.05 & rowData(pixels)$log2FoldChange > 0]
 
-wtLoops <- pixels[rowData(pixels)$padj <= 0.05 & rowData(pixels)$log2FoldChange < 0]
+diff_wtLoops <- pixels[rowData(pixels)$padj <= 0.05 & rowData(pixels)$log2FoldChange < 0]
 
 ## Initiate plotgardener page
-pageCreate(width = 4, height = 3,
+pageCreate(width = 4.1, height = 4.25,
            showGuides = F)
 
 ## Define shared parameters
 p <- pgParams(assembly = "hg19",
               resolution = 10e3,
               chrom = "9",
-              chromstart = 128399598 - 100e3,
-              chromend = 128839674 + 100e3,
-              zrange = c(0,150),
+              chromstart = 128420000,
+              chromend   = 128750000 ,
+              zrange = c(0,300),
               norm = "SCALE",
               x = 0.25,
               width = 3.5,
               length = 3.5,
-              height = 1)
-
+              height = 1.5)
 
 ## Plot WT Hi-C Mega Map
-wt_hic <- plotHicRectangle(data = megaMap_files[["megaMap_WT"]],                             
+wt_hic <- plotHicRectangle(data = megaMap_files[["megaMap_WT"]],
                            params = p,
-                           x = 0.25,
                            y = 0.25)
 
-
 ## Plot FS Hi-C Map
-fs_hic <- plotHicRectangle(data = megaMap_files[["megaMap_FS"]],                             
+fs_hic <- plotHicRectangle(data = megaMap_files[["megaMap_FS"]],
                            params = p,
-                           x = 0.25,
-                           y = 1.3)
+                           y = 1.8)
 
 ## Add legend for WT Hi-C map
 annoHeatmapLegend(plot = wt_hic,
@@ -209,59 +197,54 @@ annoHeatmapLegend(plot = wt_hic,
 ## Add legend for FS Hi-C map
 annoHeatmapLegend(plot = fs_hic,
                   x = 3.85,
-                  y = 1.3,
+                  y = 1.8,
                   width = 0.1,
                   height = 0.75,
                   fontcolor = 'black')
 
 ## Annotate loops
 annoPixels(plot = wt_hic,
-           data = interactions(fsLoops),
-           type = "arrow",
-           col = 'blue')
+           data = interactions(diff_fsLoops),
+           type = "arrow",shift = 2,
+           col = 'black')
 
 annoPixels(plot = fs_hic,
-           data = interactions(fsLoops),
-           type = "arrow",
-           col = 'blue')
-
+           data = interactions(diff_fsLoops),
+           type = "arrow",shift = 2,
+           col = 'black')
 
 ## Add text labels
 plotText(label = "WT",
-         x = 0.25,
-         y = 0.25,
+         x = 0.3,
+         y = 0.3,
          just = c('top', 'left'))
 
 plotText(label = "FS",
-         x = 0.25,
-         y = 1.3,
+         x = 0.3,
+         y = 1.85,
          just = c('top', 'left'))
 
 ## Add Genes + Gene labels
-plotGenes(width = 3.5,
-          height = 0.5, 
-          chrom = paste0("chr", p$chrom),
+plotGenes(chrom = paste0("chr", p$chrom),
           params = p,
-          x = 0.25,
-          y = 2.325)
+          height = 0.5,
+          y = 3.35)
 
 plotGenomeLabel(params = p,
                 chrom = paste0("chr", p$chrom),
-                x = 0.25,
-                y = 2.825)
-
+                y = 3.9)
 
 # Create Survey Plot ------------------------------------------------------
 
 ## Take Top 50 FS loops
-fsLoops_50 <- head(fsLoops[order(rowData(fsLoops)$padj, decreasing = F)], 50)
+fsLoops_50 <- head(diff_fsLoops[order(rowData(diff_fsLoops)$padj, decreasing = F)], 50)
 
 ## Convert to GRanges Object
 fsLoops_gr <-
-  GRanges(seqnames = as.character(seqnames(anchors(x = fsLoops, "first"))),
-          ranges = IRanges(start = start(anchors(fsLoops, "first")),
-                           end = end(anchors(fsLoops, "second"))),
-          mcols = mcols(fsLoops))
+  GRanges(seqnames = as.character(seqnames(anchors(x = fsLoops_50, "first"))),
+          ranges = IRanges(start = start(anchors(fsLoops_50, "first")),
+                           end = end(anchors(fsLoops_50, "second"))),
+          mcols = mcols(fsLoops_50))
 
 ## Add buffer 
 buffer <- 200e3
@@ -269,92 +252,87 @@ fsLoops_gr_buffer <- fsLoops_gr + buffer
 
 ## Make pdf
 pdf(file = "plots/Flores_F1000R.pdf",
-    width = 5.75,
-    height = 6)
+    width = 4.1,
+    height = 4.25)
 
 ## Loop through each region
 for(i in seq_along(fsLoops_gr_buffer)){ 
   
   ## Initiate plotgardener page
-  pageCreate(width = 5.75, height = 6,
+  pageCreate(width = 4.1, height = 4.25,
              showGuides = F)
   
   ## Define shared parameters
   p <- pgParams(assembly = "hg19",
                 resolution = 10e3,
-                chrom = as.character(seqnames(fsLoops_gr_buffer))[i],
+                chrom = as.character(seqnames(fsLoops_gr_buffer))[i], 
                 chromstart = start(fsLoops_gr_buffer)[i],
-                chromend = end(fsLoops_gr_buffer)[i],
-                zrange = c(0,150),
+                chromend   = end(fsLoops_gr_buffer)[i],
+                zrange = c(0,300),
                 norm = "SCALE",
                 x = 0.25,
-                width = 5,
-                length = 5,
-                height = 2)
+                width = 3.5,
+                length = 3.5,
+                height = 1.5)
   
   ## Plot WT Hi-C Mega Map
-  wt_hic <- plotHicRectangle(data = megaMap_files[["megaMap_WT"]],                             
+  wt_hic <- plotHicRectangle(data = megaMap_files[["megaMap_WT"]],
                              params = p,
-                             x = 0.25,
                              y = 0.25)
   
   ## Plot FS Hi-C Map
-  fs_hic <- plotHicRectangle(data = megaMap_files[["megaMap_FS"]],                             
+  fs_hic <- plotHicRectangle(data = megaMap_files[["megaMap_FS"]],
                              params = p,
-                             x = 0.25,
-                             y = 2.3)
+                             y = 1.8)
   
   ## Add legend for WT Hi-C map
   annoHeatmapLegend(plot = wt_hic,
-                    x = 5.35,
+                    x = 3.85,
                     y = 0.25,
                     width = 0.1,
-                    height = 1.5,
+                    height = 0.75,
                     fontcolor = 'black')
   
   ## Add legend for FS Hi-C map
   annoHeatmapLegend(plot = fs_hic,
-                    x = 5.35,
-                    y = 2.5,
+                    x = 3.85,
+                    y = 1.8,
                     width = 0.1,
-                    height = 1.5,
+                    height = 0.75,
                     fontcolor = 'black')
   
-  ## Annotate FS loops
+  ## Annotate loops
   annoPixels(plot = wt_hic,
              data = interactions(fsLoops_50),
-             type = "arrow",
-             col = '#DC3220')
+             type = "arrow",shift = 2,
+             col = 'black')
   
   annoPixels(plot = fs_hic,
              data = interactions(fsLoops_50),
-             type = "arrow",
-             col = '#DC3220')
+             type = "arrow",shift = 2,
+             col = 'black')
   
   ## Add text labels
   plotText(label = "WT",
-           x = 0.25,
-           y = 0.25,
+           x = 0.3,
+           y = 0.3,
            just = c('top', 'left'))
   
   plotText(label = "FS",
-           x = 0.25,
-           y = 2.35,
+           x = 0.3,
+           y = 1.85,
            just = c('top', 'left'))
   
   ## Add Genes + Gene labels
-  plotGenes(width = 5,
-            chrom = paste0("chr", p$chrom),
+  plotGenes(chrom = paste0("chr", p$chrom),
             params = p,
-            x = 0.25,
-            y = 4.25,
-            height = 1)
+            height = 0.5,
+            y = 3.35)
   
   plotGenomeLabel(params = p,
                   chrom = paste0("chr", p$chrom),
-                  x = 0.25,
-                  y = 5.25)
-}
+                  y = 3.9)
+  }
 dev.off()
 
 ## Get session information
